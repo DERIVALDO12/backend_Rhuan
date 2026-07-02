fa2dceebe416e8bd8ac83d5b15fd3eda3bd72ad6
DERIVALDO12 <rhuan.toigo@escola.pr.gov.br>
2026-07-02 13:20:23 +0000

    remove node_modules e adiciona gitignore

--- lista1/ex9.js
+++ lista1/ex9.js
@@ -2,7 +2,7 @@ const input = require('readline-sync');
 let a = input.question()
 let b = input.question()
 function ronaldoo ( a , b ) {
-    if( a > b ){
+    if( a < b ){
 return a
 
     }
@@ -16,7 +16,7 @@ console.log(ronaldoo ( a, b))
 let rhu = input.question()
 let an = input.question()
 let ronaldinho = (rhu , an ) => {
- if( rhu > an ){
+ if( rhu < an ){
 return rhu
 
     }
