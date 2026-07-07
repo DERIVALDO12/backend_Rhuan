5a9322cd866305401037757592c0efedce2c2c6f
DERIVALDO12 <rhuan.toigo@escola.pr.gov.br>
2026-07-07 11:34:56 +0000

    to tentando

---
+++ aula3/ex2.js
@@ -0,0 +1,20 @@
+const input = require('readline-sync')
+const produtos = [
+{id: 1, nome: "ps5",  preco: 1000},
+{id: 2, nome: "salgado",  preco: 10},
+{id: 3, nome: "lapis",  preco: 5000},
+{id: 4, nome: "caneta DO DIEGO",  preco: 100000}
+]
+for(let i = 0; i < produtos.length; i++){
+    console.log(produtos[i])
+}
+produtos.push({id: 5, nome:  "coxinha sem massa gigante com catupiri", preco: 1000})
+
+console.log("cardapio novo")
+
+for(let i = 0; i < produtos.length; i++){
+    console.log(produtos[i])
+}
+
+
+console.log(produtos.length)
